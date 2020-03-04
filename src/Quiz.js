b262e7d7f677889b9345482f98c1c56b64b4dce6
Jon Whitter <jon.whitter@cantarus.com>
2020-03-04 13:35:39 +0000

    first module completed

---
+++ src/Quiz.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+let quizData = require('./quiz_data.json');
+
+class Quiz extends Component {
+    constructor(props)
+    {
+        super(props);
+
+        this.state = { quiz_position: 1 }
+    }
+
+    render() {
+        return (
+        <div>
+            <div className="QuizQuestion">{quizData.quiz_questions[this.state.quiz_position].instruction_text}</div>
+        </div>)
+    }
+}
+
+export default Quiz;
