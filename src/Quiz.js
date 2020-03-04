604fa043e1efddf139d3b47c110d3d439b3125b9
Jon Whitter <jon.whitter@cantarus.com>
2020-03-04 15:20:01 +0000

    completed module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,6 +16,10 @@ class Quiz extends Component {
         this.setState({quiz_position: this.state.quiz_position + 1});
     }
 
+    handleResetClick() {
+        this.setState({quiz_position: 1});
+    }
+
     render() {
         const isQuizEnd = this.state.quiz_position - 1 == quizData.quiz_questions.length;
 
@@ -23,7 +27,7 @@ class Quiz extends Component {
             <div>
                 {
                     isQuizEnd ?
-                        <QuizEnd />
+                        <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
                         :
                         <QuizQuestion
                             quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
